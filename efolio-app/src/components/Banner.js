54fca2bca9d36ef60b5185c1c9d625060b5c24b3
Josh <lutz_143@yahoo.com>
2023-03-19 14:23:31 -0400

    minor update to color on banner

--- efolio-app/src/components/Banner.js
+++ efolio-app/src/components/Banner.js
@@ -5,7 +5,7 @@ const steelersYellow = "#ffda71";
 
 function Banner(props) {
   return (
-    <div className="top-banner" style={{background: `linear-gradient(200deg, ${steelersYellow} 0%, ${steelersYellow} 100%), url(${BannerLogo})`, backgroundRepeat: "no-repeat", backgroundSize: "cover", backgroundPosition: "center", backgroundBlendMode: "soft-light"}} alt="Banner Image">
+    <div className="top-banner" style={{background: `linear-gradient(200deg, ${steelersYellow} 0%, ${steelersYellow} 100%), url(${BannerLogo})`, backgroundRepeat: "no-repeat", backgroundSize: "cover", backgroundPosition: "center", backgroundColor: "#31343d", backgroundBlendMode: "soft-light"}} alt="Banner Image">
       <h2>Welcome!</h2>
     </div>
   );
