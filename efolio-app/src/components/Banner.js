d615001c985055161c84ddcb106fa21af33df2a3
Josh <lutz_143@yahoo.com>
2023-03-19 14:07:55 -0400

    updates to Banner.js and App.css to properly style banner logo

--- efolio-app/src/components/Banner.js
+++ efolio-app/src/components/Banner.js
@@ -1,10 +1,12 @@
 import React from "react";
 import BannerLogo from './images/banner.jpg';
 
+const steelersYellow = "#ffda71";
+
 function Banner(props) {
   return (
-    <div className="top-banner">      
-      <img src={BannerLogo} className="top-banner" alt="Banner Image"/>      
+    <div className="top-banner" style={{background: `linear-gradient(200deg, ${steelersYellow} 0%, ${steelersYellow} 100%), url(${BannerLogo})`, backgroundRepeat: "no-repeat", backgroundSize: "cover", backgroundPosition: "center", backgroundBlendMode: "soft-light"}} alt="Banner Image">
+      <h2>Welcome!</h2>
     </div>
   );
 };
