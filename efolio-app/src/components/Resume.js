0f341fc9b30913abbee9ffb42595e3529b1d8b6a
Josh <lutz_143@yahoo.com>
2023-03-22 21:44:59 -0400

    styling for resume to include borders and update date styling

--- efolio-app/src/components/Resume.js
+++ efolio-app/src/components/Resume.js
@@ -13,66 +13,61 @@ function About(props) {
       </h2>
         <div class="resume-section" id="experience">
           <div class="resume-section-content">
-            <div class="experience">Experience</div>
+            <div class="experience">EXPERIENCE</div>
             <div class="employer">
               <div>
                 <h3>Lockheed Martin
                 <p>Recipient of Various Formal Recognitions Including 6 Monetary Awards</p>
                 <p>Clearance: Top Secret</p>
                 </h3>
-                <div>
-                  <div>Manager of Special Tools & Projects, Washington D.C.</div>
+                <div class="resume-card">
+                  <div class="resume-title">Manager of Special Tools & Projects, Washington D.C.<span id="resume-date">September 2022 - Present</span></div>
                   <p>Refined leadership skills by managing a team focused on automation, data management, and user experience</p>
                   <ul>
                     <li>Oversaw progress of special projects by implementation of Kanban boards, user stories, and acceptance criteria</li>
                     <li>Led a team to develop, code, test, and deploy an Alteryx-Tableau database to enhance the executive business decision process</li>
-                  </ul>
-                  <div class="flex-shrink-0"><span class="text-primary">September 2022 - Present</span></div>
+                  </ul>                  
                 </div>
-                <div>
-                  <div>Independent Cost Evaluation Staff Analyst, Washington D.C.</div>
+                <div class="resume-card">
+                  <div class="resume-title">Independent Cost Evaluation Staff Analyst, Washington D.C.<span id="resume-date">March 2021 - September 2022</span></div>
                   <p>Expanded analytical thinking with an emphasis on statistical theory and regression analysis</p>
                   <ul>
                     <li>Developed, coded, and tested a program to analyze inputs to determine proposal delegation derived from dynamic algorithms</li>
                     <li>Utilized data modeling and regression to inform a confidence factor on program cost and performance</li>
-                  </ul>
-                  <div class="flex-shrink-0"><span class="text-primary">March 2021 - September 2022</span></div>
+                  </ul>                  
                 </div>
-                <div>
-                  <div>Senior Program Finance Analyst, Washington D.C.</div>
+                <div class="resume-card">
+                  <div class="resume-title">Senior Program Finance Analyst, Washington D.C.<span id="resume-date">July 2019 - March 2021</span></div>
                   <p>Cultivated a thorough knowledge of earned value and the efficient allocation of resources necessary for successful program performance</p>
                   <ul>
                     <li>Incorporated linear regression techniques in calculating outlooks and long range plans for increased forecasting accuracy</li>
                     <li>Introduced automated tools, processes, and checkpoints to ensure accuracy and compliance, saving hourly and direct costs</li>
-                  </ul>
-                  <div class="flex-shrink-0"><span class="text-primary">July 2019 - March 2021</span></div>
+                  </ul>                  
                 </div>
-                <div>
-                  <div>Corporate Contracts, Bethesda, MD</div>
+                <div class="resume-card">
+                  <div class="resume-title">Corporate Contracts, Bethesda, MD<span id="resume-date">January 2017 - July 2019</span></div>
                   <p>Heightened sense of business acumen through managing contractual risk and formulating data analytics for Program Assessment & Evaluation</p>
                   <ul>
                     <li>Developed, coded, and tested an Access-SQL database regarding independent cost metrics and presented the resulting analytics to the CFO and the Corporation’s leadership team</li>
                     <li>Engaged in multiple levels of the Corporation’s business pursuits, reviews, and approvals maximizing return on invested capital</li>
-                  </ul>
-                  <div class="flex-shrink-0"><span class="text-primary">January 2017 - July 2019</span></div>
+                  </ul>                  
                 </div>
-                <div>
-                  <div>Government Finance, Lakeland, FL</div>
+                <div class="resume-card">
+                  <div class="resume-title">Government Finance, Lakeland, FL<span id="resume-date">January 2016 - January 2017</span></div>
                   <p>Fostered a deeper understanding of the allocation of costs as it relates to the disclosure statement and government regulations</p>
                   <ul>
                     <li>Performed in-depth analysis in support of government negotiations regarding unallowable premiums and Incurred Cost Claims</li>
                     <li>Designated by leadership to partake in a tiger team to analyze and rectify post-implementation issues with Concur</li>
-                  </ul>
-                  <div class="flex-shrink-0"><span class="text-primary">January 2016 - January 2017</span></div>
+                  </ul>                  
                 </div>
-                <div>
-                  <div>Accounts Receivable Management, Columbus, OH</div>
+                <div class="resume-card">
+                  <div class="resume-title">Accounts Receivable Management, Columbus, OH<span id="resume-date">September 2012 - January 2016</span></div>
                   <p>Developed business acumen by using financial analytical skills to identify process inefficiencies and form procedures to improve upon them</p>
                   <ul>
                     <li>Held positions of increasing rank by accurately forecasting, modeling, and reconciling receivables</li>
                     <li>Coded automated checkpoints to identify discrepancies between databases and source systems to relieve current resources</li>
                   </ul>
-                  <div class="flex-shrink-0"><span class="text-primary">September 2012 - January 2016</span></div>
+                  
                 </div>
               </div>
             </div>                      
